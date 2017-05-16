d0a0f3a0791da835d6fe4cedcf7036bb173c09e9
Abhinav Gangwar <abhgang@gmail.com>
2017-05-16 15:51:51 +0530

    Adding extra files - for testing purpose

--- moc_FileActions.cpp
+++ moc_FileActions.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_FileActions_t {
-    QByteArrayData data[9];
-    char stringdata0[94];
+    QByteArrayData data[10];
+    char stringdata0[106];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -39,12 +39,13 @@ QT_MOC_LITERAL(4, 38, 8), // "contents"
 QT_MOC_LITERAL(5, 47, 13), // "sourceChanged"
 QT_MOC_LITERAL(6, 61, 6), // "source"
 QT_MOC_LITERAL(7, 68, 12), // "readContents"
-QT_MOC_LITERAL(8, 81, 12) // "saveContents"
+QT_MOC_LITERAL(8, 81, 12), // "saveContents"
+QT_MOC_LITERAL(9, 94, 11) // "addContents"
 
     },
     "FileActions\0contentsChanged\0\0QString&\0"
     "contents\0sourceChanged\0source\0"
-    "readContents\0saveContents"
+    "readContents\0saveContents\0addContents"
 };
 #undef QT_MOC_LITERAL
 
@@ -54,22 +55,23 @@ static const uint qt_meta_data_FileActions[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
-       2,   42, // properties
+       5,   14, // methods
+       2,   50, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,   34,    2, 0x06 /* Public */,
-       5,    1,   37,    2, 0x06 /* Public */,
+       1,    1,   39,    2, 0x06 /* Public */,
+       5,    1,   42,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       7,    0,   40,    2, 0x08 /* Private */,
+       7,    0,   45,    2, 0x08 /* Private */,
 
  // methods: name, argc, parameters, tag, flags
-       8,    0,   41,    2, 0x02 /* Public */,
+       8,    0,   46,    2, 0x02 /* Public */,
+       9,    1,   47,    2, 0x02 /* Public */,
 
  // signals: parameters
     QMetaType::Void, 0x80000000 | 3,    4,
@@ -80,6 +82,7 @@ static const uint qt_meta_data_FileActions[] = {
 
  // methods: parameters
     QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    4,
 
  // properties: name, type, flags
        4, QMetaType::QString, 0x00495103,
@@ -102,6 +105,7 @@ void FileActions::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
         case 1: _t->sourceChanged((*reinterpret_cast< QString(*)>(_a[1]))); break;
         case 2: _t->readContents(); break;
         case 3: _t->saveContents(); break;
+        case 4: _t->addContents((*reinterpret_cast< QString(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -171,13 +175,13 @@ int FileActions::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 5;
     }
 #ifndef QT_NO_PROPERTIES
    else if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
